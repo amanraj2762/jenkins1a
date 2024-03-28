330e4f55d06ffee481394e8b04e7993b640efd71
Aman Raj <77634872+amanraj2762@users.noreply.github.com>
2024-03-28 12:17:15 +0530

    Update firstTest.java

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -15,4 +15,13 @@ public class firstTest {
 		assertEquals(t1,t2);
 	}
 
+	@Test
+	public void test1() {
+		
+		multiply obj1 = new multiply();
+		int t1=obj.mul(8,2);
+		int t2=10;
+		assertEquals(t1,t2);
+	}
+
 }
